eb0c86d22cdb0bb353900df156d9bd196bf9e3d0
Erik Sacre <erik.sacre@gmail.com>
2018-08-29 15:22:42 +0200

    No need for all those module declarations.

--- BankKataFs.Domain/Library.fs
+++ BankKataFs.Domain/Library.fs
@@ -3,22 +3,19 @@ namespace BankKataFs.Domain
 type Date = string
 type Amount = int
 
-module Console =
-    type PrintLine = string -> unit
+type PrintLine = string -> unit
     
-module Repository =
-    type Add = (Date * Amount) -> unit
+type Add = (Date * Amount) -> unit
     
-module Clock =
-    type GetToday = unit -> Date
+type GetToday = unit -> Date
 
 module Account =
-    let Deposit (getToday: Clock.GetToday) (add: Repository.Add) (amount: Amount) =
+    let Deposit (getToday: GetToday) (add: Add) (amount: Amount) =
         add (getToday (), amount)
         
-    let Withdraw (getToday: Clock.GetToday) (add: Repository.Add) (amount: Amount) =
+    let Withdraw (getToday: GetToday) (add: Add) (amount: Amount) =
         add (getToday (), -amount)
         
-    let PrintStatement (printLine: Console.PrintLine) =
+    let PrintStatement (printLine: PrintLine) =
         ()
         
