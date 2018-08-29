609a6c12fd9e629e0a6742a84a45bfbb0adb616a
Erik Sacre <erik.sacre@gmail.com>
2018-08-29 17:07:47 +0200

    Completed StatementPrinter

---
+++ BankKataFs.Tests/StatementPrinterTests.fs
@@ -0,0 +1,32 @@
+module BankKataFs.Tests.StatementPrinterTests
+
+open NUnit.Framework
+open FsUnit
+open BankKataFs.Domain
+
+module StatementPrinterTests =
+    open BankKataFs.Tests
+
+    let [<Test>] ``Prints the header if there are no transactions`` () =
+        let (printLine, printed) = Console.Capture ()
+        let print = StatementPrinter.Print printLine
+        
+        print []
+        
+        printed () |> should equal [ "DATE | AMOUNT | BALANCE" ]
+
+    let [<Test>] ``Prints all transactions in reverse chronological order including a running balance`` () =
+        let (printLine, printed) = Console.Capture ()
+        let print = StatementPrinter.Print printLine
+        
+        print [
+            ("01/01/2018", 100);
+            ("02/01/2018", -50);
+            ("03/01/2018", 200)
+        ]
+        
+        printed () |> should equal [
+            "DATE | AMOUNT | BALANCE";
+            "03/01/2018 | 200.00 | 250.00";
+            "02/01/2018 | -50.00 | 50.00";
+            "01/01/2018 | 100.00 | 100.00" ]
